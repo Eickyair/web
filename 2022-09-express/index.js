43f56669f3c99fcf11f82ddb7a679f445d90ee8c
anvil <martinezerickaguilar@comunidad.unam.mx>
2022-09-11 09:42:54 -0500

    Parametros en express

--- 2022-09-express/index.js
+++ 2022-09-express/index.js
@@ -17,17 +17,32 @@ app.post('/user', (req, res) => {
     res.send("Nuevo usuario creado")
     console.log(req.body) //envio de informacion
 })
+//+ extraer desde la ruta los parametros PARAMS
+app.get('/hello/:user', (req, res) => {
+    console.log(typeof req.params.user) // saber el tipo de dato nos permite saber que metodos utilizar
+    res.send(`Hello ${req.params.user.toUpperCase()}`)
+})
 
-/**
- * Cuando enviamos una peticion pose un contenido y
- * CABACERAS | headers :json
- *  -statusCode
- * Es metainformacion
- * ENDPOINT
- *  ruta desde donde se hizo la peticion
- * REQ
- *  es donde se guarda la informacion del cliente
- */
+app.get('/suma/:a/:b', (req, res) => {
+    let { a, b } = req.params
+    a = parseInt(a)
+    b = parseInt(b)
+    res.send(`Resultado: ${a+b}`)
+})
+
+app.get('/users/:username/photo', (req, res) => {
+    if(req.params.username === "Anvil"){
+        return res.sendFile("./cpcfi-logo.png",{
+            root: __dirname
+        })
+    }
+    res.send('el usuario no tiene permisos')
+})
+
+app.get('/nombre/:nombre/edad/:edad',(req,res)=>{
+    res.send(`El usuario ${req.params.nombre} tiene ${req.params.edad}`)    
+})
+
 
 
 app.listen(3000);
