f383117e295128cd0ea30a42accfbb622e758df6
anvil <martinezerickaguilar@comunidad.unam.mx>
2022-09-10 23:29:36 -0500

    Response body

--- 2022-09-express/index.js
+++ 2022-09-express/index.js
@@ -1,43 +1,34 @@
 /* author:https://www.youtube.com/c/FaztTech
 video: https://www.youtube.com/watch?v=JmJ1WUoUIK4&t=153s
 */
+
+
+
+
 const express = require("express");
-const { appendFile } = require("fs");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-// envio de archivos por parte del servidor al cliente
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./cpcfi-logo.png", {
-    root: __dirname,
-  });
-});
 
-//formato de datos json
+app.use(express.text()) // con esto nuestra app puede entender texto del cliente
+app.use(express.json())// procesar jsons
+app.use(express.urlencoded({extended:false})) // manejo de datos desde un formulario
 
-app.get("/user", (req, res) => {
-  //no es necesario usar las dobles comillas
-  res.json({
-    name: "anvil",
-    address: {
-      city: "new york",
-      street: "calle 123",
-    },
-    age: 30,
-    puntos: [1, 2, 3],
-  });
-});
-//-mr 204 - todo fue bien pero no voy a devolverte nada de contenido
-// el navegador utiliza una cache para no hacer peticiones en cada momenta
-
-
-app.get('/isAlive', (req, res) => {
-    // ruta que utilizan muchos servidores para notificar que estan funcionando
-    res.sendStatus(204)
-    // sola mando un codigo de estado
+app.post('/user', (req, res) => {
+    res.send("Nuevo usuario creado")
+    console.log(req.body) //envio de informacion
 })
+
+/**
+ * Cuando enviamos una peticion pose un contenido y
+ * CABACERAS | headers :json
+ *  -statusCode
+ * Es metainformacion
+ * ENDPOINT
+ *  ruta desde donde se hizo la peticion
+ * REQ
+ *  es donde se guarda la informacion del cliente
+ */
+
 
 app.listen(3000);
 console.log("Server on port", 3000);
